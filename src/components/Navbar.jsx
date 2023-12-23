0659c0eaa1dc7d9de3b1adf967e0656521d11ef5
suraj maheshwari <surajmaheshwari159@gmail.com>
2023-12-24 00:53:45 +0530

    next commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { MdOutlineTune } from "react-icons/md";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { BiSolidMoon } from "react-icons/bi";
@@ -6,6 +6,7 @@ import { BiSolidMoon } from "react-icons/bi";
 const Navbar = ({ onDisplayOptionsClick }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [arrowRotation, setArrowRotation] = useState(0);
+  const dropdownRef = useRef(null);
 
   const toggleDropdown = () => {
     onDisplayOptionsClick();
@@ -13,9 +14,23 @@ const Navbar = ({ onDisplayOptionsClick }) => {
     setArrowRotation((prevRotation) => (prevRotation === 0 ? 180 : 0));
   };
 
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsDropdownOpen(false);
+      onDisplayOptionsClick();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav className="flex justify-between items-center px-5 py-3 border border-b-1">
-      <div className="">
+      <div className="" ref={dropdownRef}>
         <button
           onClick={toggleDropdown}
           className="inline-flex items-center gap-2 px-4 py-2 border border-gray-100 rounded-md shadow-md text-sm font-small text-gray-700 bg-white focus:outline-none transition-transform duration-300 ease-in-out"
